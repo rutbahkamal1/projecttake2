eab304cd1201f51bf11e40a38da588b959e3b96e
zarakamal <rutbahkamal@gmail.com>
2022-06-16 19:50:39 -0400

    finished project

--- projecttake2/ViewController2.swift
+++ projecttake2/ViewController2.swift
@@ -2,25 +2,30 @@
 //  ViewController2.swift
 //  projecttake2
 //
-//  Created by zara kamal on 6/14/22.
+//  Created by zara kamal on 6/16/22.
 //
 
-
 import UIKit
-import WebKit
 
 class ViewController2: UIViewController {
-    
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Do ar loading the view.
-    }
-    @IBAction func buttonTapped(_ sender: Any) {
-        let elURL = URL(string: "https://www.esteelauder.com/product/689/77491/Product-Catalog/Skincare/Repair-Serum/Advanced-Night-Repair-Serum/Synchronized-Multi-Recovery-Complex?size=3.9_oz.&gclid=Cj0KCQjw-pCVBhCFARIsAGMxhAcbOFGK-ioHsAq-ANTaJxU_Z85t6KbwsxktM0vbpzo8po1CPmSR1MYaAnkgEALw_wcB&gclsrc=aw.ds")
-        UIApplication.shared.open(elURL!)
+
+        // Do any additional setup after loading the view.
     }
     
+    let elURL = URL(string: "https://www.esteelauder.com/product/689/77491/Product-Catalog/Skincare/Repair-Serum/Advanced-Night-Repair-Serum/Synchronized-Multi-Recovery-Complex?size=3.9_oz.&gclid=Cj0KCQjw-pCVBhCFARIsAGMxhAcbOFGK-ioHsAq-ANTaJxU_Z85t6KbwsxktM0vbpzo8po1CPmSR1MYaAnkgEALw_wcB&gclsrc=aw.ds")
+    UIApplication.shared.open(elURL!)
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
